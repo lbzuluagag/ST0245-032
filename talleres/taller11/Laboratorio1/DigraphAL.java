6bc50357cd466d862ad881e99db22221c3f245f6
lbzuluagag <35717619+lbzuluagag@users.noreply.github.com>
2018-04-24 20:56:30 -0500

    aaa

--- talleres/taller11/Laboratorio1/DigraphAL.java
+++ talleres/taller11/Laboratorio1/DigraphAL.java
@@ -1,30 +1,40 @@
+
 import java.util.ArrayList;
-/**
- * Esta clase es una implementación de un digrafo usando listas de adyacencia
- * 
- * @author Mauricio Toro 
- * @version 1
- */
+import java.util.LinkedList;
+import javafx.util.Pair;
 public class DigraphAL extends Graph
 {
-   
-   public DigraphAL(int size)
-   {
-     
-   }
-   
-   public void addArc(int source, int destination, int weight)
-   {
-     
-   }
-   
-   public int getWeight(int source, int destination)
-   {
-      
-   }
-  
-   public ArrayList<Integer> getSuccessors(int vertice)
-   {
-   
-   }
+LinkedList<LinkedList<Pair<Integer,Integer>>> listas;
+public DigraphAL(int size)
+{
+    super(size);
+listas = new LinkedList<LinkedList<Pair<Integer,Integer>>>();
+for (int i = 0; i< size; i++)
+listas.add(new LinkedList<Pair<Integer,Integer>>());
+}
+
+public void addArc(int source, int destination, int weight)
+{
+listas.get(source).add(new Pair(destination, weight));
+}
+
+public int getWeight(int source, int destination)
+{
+//Error comun
+//return listas.get(source).get(destination);
+LinkedList<Pair<Integer,Integer>> listaQueVaALaDerecha = listas.get(source);
+for(Pair<Integer,Integer> pareja: listaQueVaALaDerecha)
+if (pareja.getKey() == destination)
+return pareja.getValue(); 
+return 0;
+}
+
+public ArrayList<Integer> getSuccessors(int vertice)
+{
+    ArrayList<Integer> listaa=new ArrayList<>();
+LinkedList<Pair<Integer,Integer>> listaQueVaALaDerecha = listas.get(vertice);
+for(Pair<Integer,Integer> pareja: listaQueVaALaDerecha)
+listaa.add(pareja.getValue());
+return listaa; 
+}
 }
